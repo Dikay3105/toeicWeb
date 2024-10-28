9e2e2e26353afc60cced4ec3061c9551ea3f5dae
Khoa910 <khoa9102003@gmail.com>
2024-10-28 22:02:02 +0700

    up

---
+++ ToeicWeb.Server/FeedbackService/Data/FeedbackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ToeicWeb.Server.AuthService.Models;
+
+namespace ToeicWeb.Server.AuthService.Data
+{
+	public class FeedbackDbContext : DbContext
+	{
+		public FeedbackDbContext(DbContextOptions<FeedbackDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Comment> Comments { get; set; }
+	}
+}
---
+++ ToeicWeb.Server/FeedbackService/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace ToeicWeb.Server.FeedbackService.Models
+{
+	public class Comment
+	{
+		public int UserID { get; set; }
+		public int PartID { get; set; }
+		public string CommentText { get; set; }
+		public DateTime CommentDate { get; set; }
+		public string ImgAccount { get; set; }
+	}
+}
